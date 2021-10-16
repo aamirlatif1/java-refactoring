635f058d58caddbcd74e6b5d992ed3f6313213cd
Aamir Latif <aamirlatif@gmail.com>
2021-10-16 16:26:04 +0200

    extracted methods amountFor and playFor

--- src/main/java/com/uf/invoice/InvoiceGenerator.java
+++ src/main/java/com/uf/invoice/InvoiceGenerator.java
@@ -34,41 +34,49 @@ public class InvoiceGenerator {
             for (Performance perf : invoice.performances) {
                 if (!plays.containsKey(perf.playID))
                     throw new IllegalArgumentException("unknown type: " + perf.playID);
-                final var play = plays.get(perf.playID);
-                var thisAmount = 0.0;
 
-                switch (play.type) {
-                    case "tragedy":
-                        thisAmount = 40000.0;
-                        if (perf.audience > 30) {
-                            thisAmount += 1000 * (perf.audience - 30);
-                        }
-                        break;
-                    case "comedy":
-                        thisAmount = 30000.0;
-                        if (perf.audience > 20) {
-                            thisAmount += 10000 + 500 * (perf.audience - 20);
-                        }
-                        thisAmount += 300 * perf.audience;
-                        break;
-                    default:
-                        throw new IllegalArgumentException("unknown type: " + play.type);
-                }
+                double thisAmount = amountFor(perf, playFor(perf));
 
                 // add volume credits
                 volumeCredits += Math.max(perf.audience - 30, 0);
                 // add extra credit for every ten comedy attendees
-                if ("comedy".equals(play.type))
+                if ("comedy".equals(playFor(perf).type))
                     volumeCredits += perf.audience / 5;
 
                 // print line for this order
-                result += format(" %s: %s (%d seats)\n", play.name, currency.format(thisAmount / 100.0), perf.audience);
+                result += format(" %s: %s (%d seats)\n", playFor(perf).name, currency.format(thisAmount / 100.0), perf.audience);
                 totalAmount += thisAmount;
             }
 
             result += format("Amount owed is %s\n", currency.format(totalAmount / 100.0));
             result += format("You earned %d credits\n", volumeCredits);
             return result;
+        }
+
+        private Play playFor(Performance perf) {
+            return plays.get(perf.playID);
+        }
+
+        private double amountFor(Performance perf, Play play) {
+            var thisAmount = 0.0;
+            switch (play.type) {
+                case "tragedy":
+                    thisAmount = 40000.0;
+                    if (perf.audience > 30) {
+                        thisAmount += 1000 * (perf.audience - 30);
+                    }
+                    break;
+                case "comedy":
+                    thisAmount = 30000.0;
+                    if (perf.audience > 20) {
+                        thisAmount += 10000 + 500 * (perf.audience - 20);
+                    }
+                    thisAmount += 300 * perf.audience;
+                    break;
+                default:
+                    throw new IllegalArgumentException("unknown type: " + play.type);
+            }
+            return thisAmount;
         }
     }
 }
